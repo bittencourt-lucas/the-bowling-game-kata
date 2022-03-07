114b45a73a6c3127cf89e24530031fecd944b5ff
Lucas Bittencourt <lucas.bittencourt@optelgroup.com>
2022-03-07 20:11:14 -0300

    Refactored for loops on tests

--- BowlingGame.spec.ts
+++ BowlingGame.spec.ts
@@ -1,23 +1,25 @@
 import { Game } from './BowlingGame';
 
-describe("The Bowling Game Kata", () => {
-  let g: Game;
+let g: Game;
 
+function rollMany(n: number, pins: number): void {
+  for (let i: number = 0; i < n; i++) {
+    g.roll(pins);
+  }
+}
+
+describe("The Bowling Game Kata", () => {
   beforeEach(() => {
     g = new Game();
   });
 
   it('should roll a gutter game', () => {
-    for (let i: number = 0; i < 20; i++) {
-      g.roll(0);
-    }
+    rollMany(20, 0);
     expect(g.score()).toBe(0);
   });
 
   it('should roll all ones', () => {
-    for (let i: number = 0; i < 20; i++) {
-      g.roll(1);
-    }
+    rollMany(20, 1);
     expect(g.score()).toBe(20);
   });
 });
