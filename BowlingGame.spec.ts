dde9438a6b5427d5f2cc3317f592be7d9b8c298c
Lucas Bittencourt <lucas.bittencourt@optelgroup.com>
2022-03-07 20:07:00 -0300

    Second test created and failing

--- BowlingGame.spec.ts
+++ BowlingGame.spec.ts
@@ -8,4 +8,12 @@ describe("The Bowling Game Kata", () => {
     }
     expect(g.score()).toBe(0);
   });
+
+  it('should roll all ones', () => {
+    let g: Game = new Game();
+    for (let i: number = 0; i < 20; i++) {
+      g.roll(1);
+    }
+    expect(g.score()).toBe(20);
+  });
 });
